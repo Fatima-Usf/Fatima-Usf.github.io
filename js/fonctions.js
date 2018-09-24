fc0be9bba9686d8cc60ec121ebb32165ded34a72
Fatima-YF <f.ucf.eva@gmail.com>
2018-09-24 22:42:48 +0200

    commented JQuery animation code

--- js/fonctions.js
+++ js/fonctions.js
@@ -11,11 +11,13 @@ $(window).scroll(function(){
             'transition': 'transform 2s'
         })
     */
-    if (scroll > $('.logos-pics').offset().top -($(window).height() / 1.2)) {
-        
+    /** si le scroll de la feneitre est plus grand que le débaut des images en haut */
+    if (scroll > $('.logos-pics').offset().top -($(window).height() / 1.2)) { //pour ne pas pas trop attendre je vais soustraire la moitié de la page sur n'importe quel navigateur
+        //selectionné chaque figure(contient une photo) individuellement 
         $('.logos-pics figure').each(function(i){
-
+            
             setTimeout(function(){
+                // ne fais rien pour un moment ensuite pour la figure i ajoute la classe show que je vais faire son style
                 $('.logos-pics figure').eq(i).addClass('show'); 
             }, 150 *(i+1));
            
