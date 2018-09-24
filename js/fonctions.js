ff6854852599f606bb3cd72e23a98312ee8266d2
Fatima-YF <f.ucf.eva@gmail.com>
2018-09-24 20:36:50 +0200

    added an awesome animation to logos pictures <3

--- js/fonctions.js
+++ js/fonctions.js
@@ -6,14 +6,22 @@ $(window).scroll(function(){
             'transform': 'skewY(180deg)',
             'transition': 'transform 2s'
         })
-     /*   $('.logo').css({
+    /*   $('.logo').css({
             'transform': 'translate(-200px, 0px)',
             'transition': 'transform 2s'
         })
-       
+    */
+    if (scroll > $('.logos-pics').offset().top -($(window).height() / 1.2)) {
         
-*/
-      
+        $('.logos-pics figure').each(function(i){
+
+            setTimeout(function(){
+                $('.logos-pics figure').eq(i).addClass('show'); 
+            }, 150 *(i+1));
+           
+        });
+    }
+
     }
     
     
