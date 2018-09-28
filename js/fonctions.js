dd409f871ef87576a3a3bf55ebd9ee629dcf9e49
Fatima-YF <f.ucf.eva@gmail.com>
2018-09-28 16:10:36 +0200

    trying to fix some jquery problems

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,43 +1,46 @@
-$(window).scroll(function(){
 
-    var scroll = $(this).scrollTop();
-    if (scroll/2) {
-        $('h2').css({
-            'transform': 'skewY(180deg)',
-            'transition': 'transform 2s'
-        })
-    /*   $('.logo').css({
-            'transform': 'translate(-200px, 0px)',
-            'transition': 'transform 2s'
-        })
-    */
-    /** si le scroll de la feneitre est plus grand que le débaut des images en haut */
-    if (scroll > $('.logos-pics').offset().top -($(window).height() / 1.2)) { //pour ne pas pas trop attendre je vais soustraire la moitié de la page sur n'importe quel navigateur
-        //selectionné chaque figure(contient une photo) individuellement 
-        $('.logos-pics figure').each(function(i){
+$(document).ready(function () {
+    $(window).scroll(function(){
+
+        var scroll = $(this).scrollTop();
+        if (scroll/2) {
+            $('h2').css({
+                'transform': 'skewY(180deg)',
+                'transition': 'transform 2s'
+            })
+        /*   $('.logo').css({
+                'transform': 'translate(-200px, 0px)',
+                'transition': 'transform 2s'
+            })
+        */
+        /** si le scroll de la feneitre est plus grand que le débaut des images en haut */
+        if (scroll > $('.logos-pics').offset().top -($(window).height() / 1.2)) { //pour ne pas pas trop attendre je vais soustraire la moitié de la page sur n'importe quel navigateur
+            //selectionné chaque figure(contient une photo) individuellement 
+            $('.logos-pics figure').each(function(i){
+                
+                setTimeout(function(){
+                    // ne fais rien pour un moment ensuite pour la figure i ajoute la classe show que je vais faire son style
+                    $('.logos-pics figure').eq(i).addClass('show'); 
+                }, 170 *(i+1));
             
-            setTimeout(function(){
-                // ne fais rien pour un moment ensuite pour la figure i ajoute la classe show que je vais faire son style
-                $('.logos-pics figure').eq(i).addClass('show'); 
-            }, 170 *(i+1));
-           
-        });
-    }
-
-    }
-    
-    if(scroll > $('.back-two').offset().top -$(window).height()){
-        $('.back-two').css({ 'background-position': 'center' + (scroll - $('.back-two').offset().top) +'px'})
+            });
+        }
+        }
         
-        var opacity = (scroll - $('.back-two').offset().top +400 ) / (scroll /5 )
+        if(scroll > $('.back-two').offset().top -$(window).height()){
+            $('.back-two').css({ 'background-position': 'center' + (scroll - $('.back-two').offset().top) +'px'})
+            
+            var opacity = (scroll - $('.back-two').offset().top +400 ) / (scroll /5 )
 
-        $('.love').css({'opacity': opacity})
-    }
+            $('.love').css({'opacity': opacity})
+        }
 
-    if (scroll > $('.blogs').offset().top - $(window).height()) {
-        var offset = Math.min(0, scroll - $('.blogs').offset().top +$(window).height()-360);
+        if (scroll > $('.blogs').offset().top - $(window).height()) {
+            var offset = Math.min(0, scroll - $('.blogs').offset().top +$(window).height()-360);
 
-        $('.post1').css({'transform':'translate('+ offset +'px, 20px)' });
-        $('.post3').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, 20px)' });
-    }
-})
+            $('.post1').css({'transform':'translate('+ offset +'px, 20px)' });
+            $('.post3').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, 20px)' });
+        }
+    })
+
+});
