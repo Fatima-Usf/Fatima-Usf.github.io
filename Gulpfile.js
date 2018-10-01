9cf9a1984ad9db86851072ac1fdff27fd2110adc
Fcmam5 <dr.fcmam5@gmail.com>
2018-10-01 21:25:54 +0100

    build($gulp): add gulp to devDependencies and add image optimizer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('pug',function(){
-    gulp.src('src/pug/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('www'));
+gulp.task('pug', function () {
+  gulp.src('src/pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('www'));
 });
 
-gulp.task('watch',function(){
-    gulp.watch('src/pug/*', ['pug']);
+gulp.task('images', function () {
+  gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('images'))
+}
+);
+
+gulp.task('watch', function () {
+  gulp.watch('src/pug/*', ['pug']);
 });
 
-gulp.task('default',['pug','watch']);
+gulp.task('default', ['pug', 'images', 'watch']);
